ceb27ff2a7c1ca33c2937320765b9d3517d884b0
MassylBoularas <massylboularas@gmail.com>
2025-03-03 13:46:03 -0500

    ajout route dans vercel.json

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 import NavBar from "./components/Nav/NavBar";
 import { getServerSession } from "next-auth";
